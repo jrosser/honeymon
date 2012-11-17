430c53172f008903464889d11d0d909ea7039ccd
Jonathan Rosser <jonathan.rosser@gmail.com>
2012-11-17 22:52:29 +0000

    add some comments to ascii parser

--- parserascii.cpp
+++ parserascii.cpp
@@ -5,7 +5,7 @@ ParserAscii::ParserAscii()
     state=IDLE;
 }
 
-//accept text input messages of the form
+//accept lines of text input messages of the form
 //53 A9 6A A9 AA 5A 55 96 A9 AA 9A AA 95 69 A9 A5 A9 99 AA AA A6 AA A6 96 6A A9 55 35
 void ParserAscii::inputBytes(QByteArray in)
 {
@@ -31,19 +31,25 @@ void ParserAscii::decodeChar(char c)
 
         case HEAD:
             if(c=='3') state=PAYLOAD1;
+            //prefix the message as it would have been
+            //from the binary data
             data.append(0x33);
             data.append(0x55);
             data.append(0x53);
             break;
 
         case PAYLOAD1:
+            //first character of payload
             byte = (c - '0') * 16;
             state = PAYLOAD2;
             break;
 
         case PAYLOAD2:
+            //second character of payload
             byte += (c - '0');
             data.append(byte);
+
+            //look for end of message
             if(byte==0x35) {
                 emit outputBytes(data);
                 data.clear();
